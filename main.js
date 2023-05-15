71a2f1187b720984ae197f004e1d44bd8116e2a2
austin-jarrett-dev <austin.jarrett.dev@gmail.com>
2023-05-15 13:56:26 -0500

    Delete task functionality Fixes #5

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+const deleteBtn = `<button class="deleteTaskBtn" onclick="deleteTask(this)">Delete Task</button>`;
+
 function addTask() {
-    var task = document.getElementById("taskInput").value;
+  var task = document.getElementById("taskInput").value;
+  
+  if (task != "") {
     var taskList = document.getElementById("taskList");
-    
+
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(task));
+    li.innerHTML += deleteBtn;
     taskList.appendChild(li);
 
     document.getElementById("taskInput").value = "";
+  }
+}
+
+function deleteTask(task) {
+  task.parentElement.remove();
 }
