b59f7fdf6bd69638ed0a47d4a1481f95da2244fa
austin-jarrett-dev <austin.jarrett.dev@gmail.com>
2023-05-15 22:51:09 -0500

    Mark task as complete functionality Fixes #6

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const deleteBtn = `<button class="deleteTaskBtn" onclick="deleteTask(this)">Delete Task</button>`;
+const deleteBtn = `<button class="todo-list__task--delete-btn" onclick="deleteTask(this)">Delete Task</button>`;
 
 function addTask() {
   var task = document.getElementById("taskInput").value;
-  
+
   if (task != "") {
     var taskList = document.getElementById("taskList");
 
@@ -11,10 +11,18 @@ function addTask() {
     li.innerHTML += deleteBtn;
     taskList.appendChild(li);
 
+    li.addEventListener("click", function () {
+      markTaskAsDone(li);
+    });
+
     document.getElementById("taskInput").value = "";
   }
 }
 
 function deleteTask(task) {
   task.parentElement.remove();
+}
+
+function markTaskAsDone(task) {
+  task.classList.toggle("done");
 }
